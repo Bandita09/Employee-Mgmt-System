98225336ba349a68b8de9d88af323ca95f3bc268
Bandita09 <51449881+Bandita09@users.noreply.github.com>
2020-04-11 23:15:02 +0530

    Required changes

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+  <title>Login</title>
+  <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+   <div class="login-page">
+      <div class="form">
+         <form class="login-form col">
+            <input type="text" placeholder="Enter your username"/>
+            <input type="password" placeholder="Enter your password"/>
+            <button>Log in</button>
+            <p class="message">Not registered? <a href="register.php">Register Yourself!</a></p>
+         </form>
+      </div>
+   </div>
+</body>
+</html>
