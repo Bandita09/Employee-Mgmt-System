c4f831b961dbd59b8cbab228d20a122a0474e597
ezazakhtar <ezazakhtar1@gmail.com>
2020-04-27 15:21:58 +0530

    Anything!
    Just demo

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+	<link rel="stylesheet" href="./res/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+</head>
+<body>
+	<nav class="navbar ">
+		<img src="./images/empLogo.png" class="navbar-brand">
+		Anything!
+	</nav>
+</body>
+</html>
