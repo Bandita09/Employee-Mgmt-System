95c89ad10b73863cccb61a27d3fadb0bcef424a8
ezazakhtar <ezazakhtar1@gmail.com>
2020-04-27 15:36:12 +0530

    Blue Color
    Blue color  Nav Bar!

--- index.php
+++ index.php
@@ -3,14 +3,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Index</title>
+	<title>Employee Management System!</title>
 	<link rel="stylesheet" href="./res/font-awesome-4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
 </head>
 <body>
-	<nav class="navbar ">
-		<img src="./images/empLogo.png" class="navbar-brand">
-		Anything!
+	<nav class="navbar navbar-expand-sm bg-primary navbar-light">
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="navbar-brand" href=".">Employee Management System<sup><i class="fa fa-trademark"></i></sup></a>
+			</li>
+		</ul>
 	</nav>
 </body>
 </html>
